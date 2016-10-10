a0bc0647ebd287fd4a64c7efb6de1bb7393e6e74
Alex Bielen <alexhendriebielen@gmail.com>
2016-10-09 20:46:58 -0400

    Updating to ES6 conventions and arrow functions. Upping version. Ignoring integration suite in .gitignore.

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
 /**
  * Created by alexbielen on 9/14/16.
  */
-(function () {
 
-    var Slackbot = require('slackbots');
+var Slackbot = require('slackbots');
 
-    // utility functions
-    var isChatMessage = function (message) {
-        return message.type === 'message' && Boolean(message.text);
+// utility functions
+const isChatMessage = message =>
+    message.type === 'message' && Boolean(message.text);
+
+const isChannelConvo = message =>
+    typeof message.channel === 'string' && message.channel[0] === 'C';
+
+const isFromBot = (message, botUser) =>
+    message.user === botUser.id;
+
+const mentionsTrigger = (message, settings) =>
+    settings.triggerWords.some((word) =>
+        message.text.toLowerCase().indexOf(word) > -1
+    );
+
+const getChannelById = (channelId, outerBot) =>
+    outerBot.channels.filter(item => item.id === channelId)[0];
+
+const getRandomResponse = settings =>
+    settings.responses[Math.floor(Math.random() * settings.responses.length)];
+
+
+// API
+const getBot = function (token, settings) {
+
+    const bot = new Slackbot({
+        'token': token,
+        'name': settings.botName
+    });
+
+    const botUser = bot.on('start', () =>
+        bot.users.filter(user => user.name === settings.botName)[0]
+    );
+
+    const run = function () {
+        bot.on('message', message => {
+            console.log(message);
+
+            if (isChatMessage(message) && isChannelConvo(message) && !isFromBot(message, botUser) && mentionsTrigger(message, settings)) {
+                var channel = getChannelById(message.channel, bot);
+                bot.postMessageToChannel(channel.name, getRandomResponse(settings), {as_user: true});
+            }
+        })
     };
 
-    var isChannelConvo = function (message) {
-        return typeof message.channel === 'string' && message.channel[0] === 'C';
-    };
-
-    var isFromBot = function (message, botUser) {
-        // check that message is sent from bot user
-        return message.user === botUser.id;
-    };
-
-    var mentionsTrigger = function (message, settings) {
-        // check the message against the trigger words
-        return settings.triggerWords.some(function (word) {
-            return message.text.toLowerCase().indexOf(word) > -1;
-        });
-    };
-
-    var getChannelById = function (channelId, outerBot) {
-        return outerBot.channels.filter(item => item.id === channelId)[0];
-    };
-
-    var getRandomResponse = function (settings) {
-        return settings.responses[Math.floor(Math.random() * settings.responses.length)];
-    };
-
-    // API
-    var getBot = function (token, settings) {
-        const bot = new Slackbot({
-            'token': token,
-            'name': settings.botName
-        });
-        const botUser = bot.on('start', () =>
-            bot.users.filter(user => user.name === settings.botName)[0]
-        );
-
-        var run = function () {
-            bot.on('message', message => {
-                if (isChatMessage(message) && isChannelConvo(message) && !isFromBot(message, botUser) && mentionsTrigger(message, settings)) {
-                    var channel = getChannelById(message.channel, bot);
-                    bot.postMessageToChannel(channel.name, getRandomResponse(settings), {as_user: true});
-                }
-            })
-        };
-
-        return {
-            'run': run
-        }
-    };
-
-    module.exports = {
-        'getBot': getBot
+    return {
+        'run': run
     }
-})();
+};
+
+module.exports = {
+    'getBot': getBot
+};
