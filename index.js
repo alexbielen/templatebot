28f2981d847d879d994056b0866b1220ed71c36d
Alex Bielen <abielen@newclassrooms.org>
2016-10-10 00:02:04 -0400

    Skeleton for preemptive messages aka respond when user starts typing.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var Slackbot = require('slackbots');
 
-// utility functions
+// Call and Response functions
 const isChatMessage = message =>
     message.type === 'message' && Boolean(message.text);
 
@@ -22,9 +22,23 @@ const mentionsTrigger = (message, settings) =>
 const getChannelById = (channelId, outerBot) =>
     outerBot.channels.filter(item => item.id === channelId)[0];
 
-const getRandomResponse = settings =>
-    settings.responses[Math.floor(Math.random() * settings.responses.length)];
+const getRandomResponse = responses =>
+    responses[Math.floor(Math.random() * responses.length)];
 
+
+// Preemptive Strike Functions
+
+const isSomeoneTyping = message =>
+    message.type === 'user_typing';
+
+const getUserById = (bot, userId) =>
+    bot.users.filter(user => user.id === userId)[0];
+
+
+const isUserInPreemptives = (bot, message, preemptives) => {
+    const user = getUserById(bot, message.user);
+    return user.name in preemptives;
+};
 
 // API
 const getBot = function (token, settings) {
@@ -38,14 +52,28 @@ const getBot = function (token, settings) {
         bot.users.filter(user => user.name === settings.botName)[0]
     );
 
+    const preemptiveStrikeUsers = Object.keys(settings.preemptiveStrike);
+
     const run = function () {
         bot.on('message', message => {
             console.log(message);
 
+            // Call and Response
             if (isChatMessage(message) && isChannelConvo(message) && !isFromBot(message, botUser) && mentionsTrigger(message, settings)) {
                 var channel = getChannelById(message.channel, bot);
-                bot.postMessageToChannel(channel.name, getRandomResponse(settings), {as_user: true});
+                bot.postMessageToChannel(channel.name, getRandomResponse(settings.responses), {as_user: true});
             }
+
+            // Preemptive Strike
+            if (isSomeoneTyping(message) && isUserInPreemptives(bot, message, preemptiveStrikeUsers)) {
+                var channel = getChannelById(message.channel, bot);
+                bot.postMessageToChannel(channel.name, getRandomResponse(settings), {as_user: true})
+            }
+
+
+
+
+
         })
     };
 
